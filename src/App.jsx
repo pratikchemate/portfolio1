10e18cfcf4c5e1876b8743cea7db34fbfe2bbcf3
Pratik Chemate <pratikchemate@gmail.com>
2025-08-15 16:20:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -64,10 +64,10 @@ function App() {
                 <h5>
                   A computer engineer by qualification, a coder by trade! <br />
                   <h6>
-                  I’m a passionate and adaptable developer with strong skills in Java, Python, JavaScript, SQL, and modern frameworks like React, Node.js, Express, and Spring Boot.
-I focus on building secure, scalable, and user-friendly applications, backed by solid knowledge of DSA, OOP, OS, and AI/ML basics.
-I thrive in collaborative environments, love solving complex problems, and aim to deliver clean, maintainable code that drives real-world impact.
-Open to networking, collaboration, and opportunities where technology meets innovation.
+                    <ul>
+                 <li> I’m a passionate and adaptable developer with strong skills in Java, Python, JavaScript, SQL, and modern frameworks like React, Node.js, Express, and Spring Boot.</li>
+<li>I focus on building secure, scalable, and user-friendly applications, backed by solid knowledge of DSA, OOP, OS, and AI/ML basics.</li>
+</ul>
                   </h6>
                   Based in Pune, INDIA.
                 </h5>
